4b71432b8490d5b896b1bbe5d0981be6671ef8a7
eumarcelotavares <eumarcelotavares@gmail.com>
2022-11-28 21:20:16 -0300

    código aula dia 28.11.22 V1 (exercício Get Set Data)

---
+++ src/aula281122/exercicioGetSetData/Data.java
@@ -0,0 +1,4 @@
+package aula281122.exercicioGetSetData;
+
+public class Data {
+}
---
+++ src/aula281122/exercicioGetSetData/Main.java
@@ -0,0 +1,17 @@
+//Escreva uma classe data cuja instância (objeto) representa uma data.
+//Essa classe devera dispor dos seguintes métodos:
+//construtor - define a data que determinado objeto (através de parâmetro), este método verifica se a data está correta,
+//caso não esteja a data é configurada como: 01/01/0001.
+//getDia - Retorna o DIA da data.
+//getMes - Retorna o MÊS da data.
+//getMesExtenso - Retorna o MÊS da data corrente por extenso.
+//getAno - Retorna o ANO da data.
+//isBissexto - Retorna verdadeiro se o ano da data corrente for bissexto e falso caso contrário.
+
+package aula281122.exercicioGetSetData;
+
+public class Main {
+
+
+
+}
