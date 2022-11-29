3042ccc15d18b241ce83b9e6bea3b8330529c1b9
eumarcelotavares <eumarcelotavares@gmail.com>
2022-11-28 21:37:38 -0300

    código aula dia 28.11.22 V2 (exercício Get Set Data)

--- src/aula281122/exercicioGetSetData/Data.java
+++ src/aula281122/exercicioGetSetData/Data.java
@@ -1,4 +1,32 @@
 package aula281122.exercicioGetSetData;
+import aula281122.exercicioGetSetData.Main;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class Data {
+
+    public int dia;
+    public int mes;
+    public int ano;
+
+
+//construtorMetodo - define a data que determinado objeto (através de parâmetro), este método verifica se a data está correta, caso não esteja a data é configurada como: 01/01/0001.
+    public static void construtorMetodo() {
+        System.out.println(" ");
+    }
+
+    public int getDia(int dia) {
+        this.dia = dia;
+        return (int) ChronoUnit.DAYS.between(LocalDate.of(ano, mes, dia), LocalDate.now());
+    }
+
+    public int getMes(int mes) {
+        this.mes = mes;
+        return (int) ChronoUnit.MONTHS.between(LocalDate.of(ano, mes, dia), LocalDate.now());
+    }
+
+
+
+
 }
